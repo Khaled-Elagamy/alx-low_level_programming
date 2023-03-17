a53cf40dd73f3c7dd7ed5060f233f89541db533b
Khaled-Elagamy <khaled_yasser1234@yahoo.com>
2023-03-16 19:50:07 -0700

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A Cprogram prints a aqoute iusing write function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
