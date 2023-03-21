e11cc2d8b2b050ea57b3c434adb1972f305194ec
Khaled-Elagamy <khaled_yasser1234@yahoo.com>
2023-03-21 14:40:24 -0700

    Add task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
-* print_times_table - Prints the n times table
-*
-* @n: number times table (0 < n <= 15)
-*
-* Return: no return
-*/
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
 
 void print_to_98(int n)
 {
