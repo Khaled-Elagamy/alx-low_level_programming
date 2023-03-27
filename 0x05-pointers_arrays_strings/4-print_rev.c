5040ac595fe9354217e4bce09ec72fb4171aa1d0
Khaled-Elagamy <khaled_yasser1234@yahoo.com>
2023-03-27 15:16:12 -0700

    Add fifth Task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(*s);
+
+	for (len--; len >= 0; len--)
+		_putchar(s[len]);
+	_putchar('\n');
+}
