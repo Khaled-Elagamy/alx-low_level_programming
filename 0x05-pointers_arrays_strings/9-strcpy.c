4b6c3a101ae90eb230e5f3a38204b9b6981bd741
Khaled-Elagamy <khaled_yasser1234@yahoo.com>
2023-03-27 16:46:15 -0700

    Add tenth Task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,9 +15,9 @@ char *_strcpy(char *dest, char *src)
 
 	while (src[index])
 	{
-		if (src[index] == '\0')
-			continue;
 		dest[index] = src[index];
+		if (src[index] == '\0')
+			break;
 		index++;
 	}
 	return (dest);
