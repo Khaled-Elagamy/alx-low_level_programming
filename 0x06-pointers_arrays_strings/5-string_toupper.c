b297b768fc3aafd233b28bc17028a10e4c85077e
Khaled-Elagamy <khaled_yasser1234@yahoo.com>
2023-03-30 19:09:48 -0700

    Add sixth Task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *s)
+{
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] -= 32;
+		i++;
+	}
+	return (s);
+}
